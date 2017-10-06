f18a936054be3169097baf63e5cce5fba9b7d0a9
Murtala Aliyu <murtalaliyu10@yahoo.com>
2017-10-06 17:47:30 -0400

    Add files via upload
    node_modules folder has been excluded due to its large size.

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Grid extends React.Component {
+	
+}
+
+class Main extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			generation: 0,
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>The Game of Life</h1>
+				<Grid
+				/>
+				<h2>Generations: {this.state.generation}</h2>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
